c01ec5a801e103e83ed809fad666de948b5367c7
world wide reb <ace.cochez@51tocarbonzero.com>
2025-03-06 17:12:51 +0000

    Master

---
+++ script.js
@@ -0,0 +1,11 @@
+const rateForm = document.getElementById("rate-form")
+
+const saveRating = () => {
+	console.log("Save log")
+	alert("Thank you for your input!\nYour rating has been logged.")
+}
+
+const resetRating = () => {
+	console.log("Reset log")
+	window.location.reload()
+}
